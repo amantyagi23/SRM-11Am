b3228e825df4e08f0a9d1cec2a89e0bc84cd4c8f
amantyagi23 <amantyagi4987@gmail.com>
2024-06-06 12:15:48 +0530

    6may nqueen 11

--- Nqueen.java
+++ Nqueen.java
@@ -3,36 +3,60 @@ public class Nqueen {
     boolean board[][];
 
     public int totalNQueens(int n) {
-        return;
+
+        board = new boolean[n][n];
+        return hepler(0, n);
+
     }
 
-    int helper(int row, int n) {
-        int count = 0;
+    int hepler(int row, int n) {
 
         if (row == n) {
-            count++;
-            return 1;
 
+            // System.out.println();
+            // for (int i = 0; i < board.length; i++) {
+            // for (int j = 0; j < board.length; j++) {
+            // if (board[i][j] == true) {
+            // System.out.print("Q");
+            // } else {
+            // System.out.print("-");
+            // }
+            // }
+            // System.out.println();
+            // }
+            return 1;
         }
 
+        int count = 0;
         for (int col = 0; col < n; col++) {
-
             if (willIPlaceOrNot(row, col)) {
                 board[row][col] = true;
-                helper(row + 1, n);
+                count = count + hepler(row + 1, n);
+
                 board[row][col] = false;
             }
-
         }
 
         return count;
+
     }
 
     boolean willIPlaceOrNot(int row, int col) {
 
-        //
         for (int i = row; i >= 0; i--) {
             if (board[i][col] == true) {
+                return false;
+            }
+        }
+
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == true) {
+                return false;
+            }
+        }
+
+        for (int i = row, j = col; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == true) {
                 return false;
             }
         }
@@ -41,6 +65,8 @@ public class Nqueen {
     }
 
     public static void main(String[] args) {
+        Nqueen obj = new Nqueen();
 
+        System.out.println(obj.totalNQueens(8));
     }
 }
